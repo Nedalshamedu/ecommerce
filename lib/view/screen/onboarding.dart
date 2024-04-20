38ad42965b30219f29ef0a87a96c4c3b08ba6760
Nedalshamedu <Nedalshamedu@users.noreply.github.com>
2024-04-20 22:11:59 +0300

    App Structure

---
+++ lib/view/screen/onboarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+          child: SafeArea(
+        child: Text("Hi"),
+      )),
+    );
+  }
+}
